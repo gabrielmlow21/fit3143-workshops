4b85f2ef903226a3662f8de3e6f583796181801d
Gabriel Low <lowgabriel@ymail.com>
2023-08-07 01:14:01 +0800

    added possible solution for 3b

---
+++ week3/activity3b.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define M 4
+#define N 5
+
+int main() {
+    int a[M][N] =
+        {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
+    int b[M][N] =
+        {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
+    int c[M][N] = {0};
+    int i, j;
+
+    #pragma omp parallel for private(i)
+    for (i = 0; i < M; i++) {
+        #pragma omp parallel for private(j)
+        for (j = 0; j < N; j++) {
+            c[i][j] = a[i][j] * b[i][j];
+        }
+    }
+
+    printf("Values of C:\n");
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
