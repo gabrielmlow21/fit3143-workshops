b876ede500ca7ddbebe6acd14919fe28701cdbcd
Gabriel Low <lowgabriel@ymail.com>
2023-08-07 00:42:14 +0800

    first commit

---
+++ week3/activity2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <omp.h>
+#define N 20
+
+int main() {
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int b[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    int c[N] = {0};
+    int i;
+
+    #pragma omp parallel for shared(a, b, c) private(i)
+    for (i = 0; i < N; i++) {
+        if (i > 0) {
+            #pragma omp atomic
+            c[i] = c[i - 1] + (a[i] * b[i]);
+        } else {
+            #pragma omp atomic
+            c[i] = (a[i] * b[i]);
+        }
+    }
+
+    printf("Values of C:\n");
+    for (i = 0; i < N; i++) {
+        printf("%d\t", c[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
