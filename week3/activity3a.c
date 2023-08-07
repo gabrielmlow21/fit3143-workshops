32727b899ff4e6aa2512670f5e9bb02ffd3b5245
Gabriel Low <lowgabriel@ymail.com>
2023-08-07 14:02:23 +0800

    added notes

--- week3/activity3a.c
+++ week3/activity3a.c
@@ -14,11 +14,21 @@ int main() {
 
     #pragma omp parallel for collapse(2) shared (a,b,c) private(j)
     // private is to ensure each thread has a private variable j, i is already private since it is outer loop
+    // collapse allows you to parallelize multiple loops in a nest without introducing nested parallelism
+    // n = number of loops
     for (i = 0; i < M; i++) {
         for (j = 0; j < N; j++) {
             c[i][j] = a[i][j] * b[i][j];
         }
     }
+
+    // for (i = 0; i < M; i++) {
+    //     #pragma omp parallel for
+    //     // i runs in parallel but j runs in a thread each (j threads)
+    //     for (j = 0; j < N; j++) {
+    //         c[i][j] = a[i][j] * b[i][j];
+    //     }
+    // }
 
     printf("Values of C:\n");
     for (i = 0; i < M; i++) {
